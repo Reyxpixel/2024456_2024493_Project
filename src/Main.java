66bbc2bd0e52f820c620af67f3a4a98d311dfd45
Saksham <saksham24493@iiitd.ac.in>
2025-10-04 17:44:38 +0530

    made a faux student dashboard logic, feel free to build more on your own

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,12 @@ import ui.LoginScreen;
 
 public class Main {
     public static void main(String[] args) {
-        // Initialize DB
         AuthDB.init();
 
-        // Add some hardcoded users (only if not already present)
         AuthService.register("alice", "password123", "student");
         AuthService.register("bob", "teachpass", "instructor");
         AuthService.register("charlie", "adminpass", "admin");
 
-        // Launch login screen
         new LoginScreen().setVisible(true);
     }
 }
